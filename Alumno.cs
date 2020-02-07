2c47388fa00472727b9a010304c68a6394240219
yevitb <yevitb@gmail.com>
2020-02-07 11:49:03 -0600

    Añadiendo los programas

---
+++ Alumno.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tarea2
+{
+    class Alumno : Persona
+    {
+        double proyectos;
+        double participaciones;
+        double tareas;
+        double promedio;
+        public Alumno(string nombre, string apellidoPat, string apellidoMat) : base(nombre, apellidoPat, apellidoMat) { }
+        public double Proyectos
+        {
+            get { return proyectos; }
+            set
+            {
+                if (value < 5)
+                    proyectos = (5);
+                else if (value > 10)
+                    proyectos = (10);
+                else
+                    proyectos = (value);
+            }
+        }
+        public double Tareas
+        {
+            get { return tareas; }
+            set
+            {  tareas = (value); }
+        }
+        public double Participaciones {
+            get { return participaciones; }
+            set
+            {
+                if (value > 5)
+                    participaciones = 0.8;
+                else
+                    participaciones = 0;
+            }
+        }
+        public double Promedio
+        {
+            set { promedio = proyectos + tareas + participaciones; }
+            get { return promedio; }
+            
+        }
+
+        public Alumno(string nombre, string apellidoPat, string apellidoMat,double proyectos,double tareas, double participaciones):base(nombre, apellidoPat, apellidoMat)
+        {
+            Proyectos = proyectos;
+            Tareas = tareas;
+            Participaciones = participaciones;
+
+        }
+        public void Imprimir(double promedio) 
+        {
+            Console.WriteLine("El alumno {0} {1} {2} obtuvo {3} ",Nombre,ApellidoPat,ApellidoMat,promedio);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Tarea2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nom;
+            string apP;
+            string apM;
+            double tar;
+            double part;
+            double proy;
+
+            Console.WriteLine("Alumnos a calificar ");
+            int tam = Convert.ToInt32(Console.ReadLine());
+            //int[] alumnos = new int[tam];
+            Alumno[] alumnos = new Alumno[tam];
+            for ( int i=0; i<alumnos.Length;i++)
+            {
+                Console.WriteLine("Nombre del alumno ");
+                nom = Console.ReadLine();
+                Console.WriteLine("Apellido paterno ");
+                apP = Console.ReadLine();
+                Console.WriteLine("Apellido materno ");
+                apM = Console.ReadLine();
+                Console.WriteLine("Proyecto: ");
+                proy = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Tareas: ");
+                tar = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Participaciones: ");
+                part = Convert.ToInt32(Console.ReadLine());
+                
+                alumnos[i] = new Alumno(nom,apP,apM,proy,tar,part);
+                double prom = Calculadora.Promediar(alumnos[i].Proyectos, alumnos[i].Tareas, alumnos[i].Participaciones);
+                alumnos[i].Imprimir(prom);
+                Console.ReadKey();
+            }
+        }
+    }
+}
